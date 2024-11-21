141ce865b4afdde32a9f76966ab386f2afa02175
josh kennedy <josh@outthinkgroup.com>
2024-11-21 14:21:29 -0500

    versioned

--- post-from-opengraph.php
+++ post-from-opengraph.php
@@ -8,12 +8,12 @@
  * Author URI:      YOUR SITE HERE
  * Text Domain:     post-from-opengraph
  * Domain Path:     /languages
- * Version:         0.2.2
+ * Version:         0.2.3
  *
  * @package         Post_From_Opengraph
  */
 namespace OpengraphPost;
-define("OpengraphPost_VERSION","0.2.2");
+define("OpengraphPost_VERSION","0.2.3");
 define("OpengraphPost_FILE",__FILE__);
 
 // Your code starts here.
